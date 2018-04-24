a63cec8af0af4551fcb4d2d9d0071e8bc5e127c0
IEUser <kiensengkhoo@gtoken.com>
2018-04-24 11:52:55 +0800

    upload new project
    upload new project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestingProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
